443a2b8af6c96ca5da6ad9d78eba3cec66ddce7b
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-07-18 16:45:11 +0000

    Write tests for header to footer

--- web/src/components/__tests__/footer.test.tsx
+++ web/src/components/__tests__/footer.test.tsx
@@ -1,13 +1,15 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import React from "react"
-import renderer from "react-test-renderer"
+import { render } from "@testing-library/react"
+import "@testing-library/jest-dom/extend-expect"
 
 import Footer from "../footer"
 
-describe("Footer", () => {
-  it("renders correctly", () => {
-    const tree = renderer
-      .create(<Footer siteTitle="eMpower" />)
-      .toJSON()
-    expect(tree).toMatchSnapshot()
-  })
+test(`renders the site title`, () => {
+  const siteTitle = `eMpower`
+  const { getByText } = render(<Footer siteTitle={siteTitle} />)
+  expect(getByText(/eMpower/i)).toBeInTheDocument()
 })
--- web/src/components/__tests__/header.test.tsx
+++ web/src/components/__tests__/header.test.tsx
@@ -1,13 +1,26 @@
+/**
+ * @jest-environment jsdom
+ */
+
 import React from "react"
-import renderer from "react-test-renderer"
+import { render } from "@testing-library/react"
+import "@testing-library/jest-dom/extend-expect"
 
 import Header from "../header"
 
-describe("Header", () => {
-  it("renders correctly", () => {
-    const tree = renderer
-      .create(<Header siteTitle="eMpower" />)
-      .toJSON()
-    expect(tree).toMatchSnapshot()
-  })
+test("renders the site title", () => {
+  const siteTitle = `eMpower`
+  const { container, getByText } = render(<Header siteTitle={siteTitle} />)
+  expect(getByText("eMpower")).toBeInTheDocument()
+  expect(container.firstChild).toMatchInlineSnapshot(`
+    <header>
+      <h1>
+        <a
+          href="/"
+        >
+          eMpower
+        </a>
+      </h1>
+    </header>
+  `)
 })
