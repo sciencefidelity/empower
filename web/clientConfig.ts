00b0b9b5916822254c5b16164aab00300c24e989
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-10-16 22:06:33 +0100

    refactor: remove duplicate config

--- web/clientConfig.ts
+++
@@ -1,9 +0,0 @@
-import PicoSanity from "picosanity"
-
-const Client = new PicoSanity({
-  projectId: process.env.SANITY_PROJECT_ID!,
-  dataset: process.env.SANITY_DATASET!,
-  useCdn: true
-})
-
-export default Client
