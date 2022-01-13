016b6e847431416cdf9e302abdfc13fe43e10a5d
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-01-13 18:38:36 +0000

    chore: remove old unused staging site

--- next/README.md
+++
@@ -1,3 +0,0 @@
-# eMpower
-
-This is the front end built with [Next.js](https://nextjs.org/).
--- next/lib/config.ts
+++
@@ -1,6 +0,0 @@
-export const config = {
-  dataset: process.env.SANITY_DATASET || "",
-  projectId: process.env.SANITY_PROJECT_ID || "production",
-  apiVersion: "2021-09-20",
-  useCdn: true
-}
