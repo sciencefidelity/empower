15e3ec770e058a01762cdc793c9abfcbbd547cea
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-02-12 02:43:44 +0000

    chore: upgrade and refresh all node_modules

--- dev/astro.config.mjs
+++ dev/astro.config.mjs
@@ -1,16 +1,13 @@
 import { resolve } from "path"
 
 export default {
-  // projectRoot: '.',     // Where to resolve all URLs relative to. Useful if you have a monorepo project.
-  // pages: './src/pages', // Path to Astro components, pages, and data
-  // dist: './dist',       // When running `astro build`, path to final static output
-  // public: './public',   // A folder of static files Astro will copy to the root. Useful for favicons, images, and other files that don’t need processing.
+  // https://docs.astro.build/en/reference/configuration-reference/
   buildOptions: {
-    site: 'https://empower-us-all.org',
+    site: "https://empower-us-all.org",
     sitemap: true
   },
   devOptions: {
-    // hostname: 'localhost',  // The hostname to run the dev server on.
+    // hostname: "0.0.0.0",
     port: 8080
   },
   renderers: [],
