7657650d71d79bf8e7ea10e97ae2dcfb1a482f59
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-02-13 22:29:48 +0000

    feat: start to implement an api route for sendgrid

--- dev/api/sendgrid.js
+++ dev/api/sendgrid.js
@@ -4,17 +4,36 @@ const sgMail = require("@sendgrid/mail")
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 async function sendEmail(req, res) {
+  const subject = "New Message from artsed.wales"
+  const fullName = `${req.body.firstName} ${req.body.lastName}`
   try {
-    await sgMail.send({
-      to: "hello@empower-us-all.org",
-      from: "hello@empower-us-all.org",
-      subject: "Sending with SendGrid is Fun",
-      text: "and easy to do anywhere, even with Node.js",
-      html: "<strong>and easy to do anywhere, even with Node.js</strong>"
+    // console.log("REQ.BODY", req.body)
+    await sendgrid.send({
+      to: "Matt Cook <hello@empower-us-all.org>",
+      from: "NAEN Website <hello@empower-us-all.org>",
+      replyTo: `${fullName} <${req.body.email}>`,
+      subject: `${subject}`,
+      html: `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+          <meta charset="utf-8">
+          <title>${subject}</title>
+          <meta name="description" content="${subject}">
+        </head>
+        <body>
+          <p>New message from ${fullName}.</p>
+          <p>Their email is: <a href="mailto:${req.body.email}">${req.body.email}</a></p>
+          <p>Message:</p>
+          <p>${req.body.message}</p>
+        </body>
+        </html>
+      `
     }).then(() => {
       console.log("Email sent")
     })
   } catch (error) {
+    // console.log(error)
     return res.status(error.statusCode || 500).json({ error: error.message })
   }
   return res.status(200).json({ error: "" })
