fdd7483ba59bedd41a65f6818e9e6342c52ceac0
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-02-14 12:41:37 +0000

    feat: setup contact form to connect to sendgrid api

--- dev/src/components/Contact.svelte
+++ dev/src/components/Contact.svelte
@@ -1,87 +1,99 @@
-<script>
-let firstName = ""
-let lastName = ""
-let email = ""
-let message = ""
-//let showMessage = false
-// let responseMessage = ""
-const submitForm = async () => {
-  const res = await fetch("api/sendgrid", {
-    body: JSON.stringify({
-      firstName: firstName,
-      lastName: lastName,
-      email: email,
-      message: message
-    }),
-    headers: {"Content-Type": "application/json"},
-    method: "POST"
-  })
-  // const data = await submit.json()
-  // if (data.message === "bad request") {
-  //   showMessage = true
-  //   responseMessage = "That looks like a bad request"
-  // }
-  // if (data.message === "email sent!") {
-  //   showMessage = true
-  //   responseMessage = "Your message was sent!"
-  // }
-  // if (
-  //   data.message === "something went wrong with the email submit!"
-  // ) {
-  //   showMessage = false
-  // }
+<script lang="ts">
+let fields = { firstName: "", lastName: "", email: "", message: "" }
+let errors = { firstName: "", lastName: "", email: "", message: "" }
+let valid = false
+
+const submitHandler = async () => {
+  valid = true
+  if (fields.firstName.trim().length < 1) {
+    valid = false
+    errors.firstName = "Please fill in First Name"
+  } else {
+    errors.firstName = ""
+  }
+  if (fields.lastName.trim().length < 1) {
+    valid = false
+    errors.lastName = "Please fill in Last Name"
+  } else {
+    errors.lastName = ""
+  }
+  // if (regex.test(fields.email.trim())) {
+  if (fields.email.trim().length < 1) {
+    valid = false
+    errors.email = "Please provide a valid email address"
+  } else {
+    errors.email = ""
+  }
+  if (fields.message.trim().length < 1) {
+    valid = false
+    errors.message = "Please leave us a message"
+  } else {
+    errors.message = ""
+  }
+
+  if (valid) {
+    console.log(fields)
+    await fetch("api/sendgrid", {
+      body: JSON.stringify(fields),
+      headers: {"Content-Type": "application/json"},
+      method: "POST"
+    })
+  }
 }
 </script>
 
 <section>
-<!--   {#if success}
-    <div class="text-center">
-      <h3 class="font-extrabold text-3xl">{responseMessage}</h3>
-    </div>
-  {:else} -->
-    <div>
-      <form on:submit|preventDefault={submitForm}>
-        <p>Send a message with Svelte</p>
-        <label for="firstName">First Name</label>
-        <input
-          id="firstName"
-          aria-label="First Name"
-          type="text"
-          bind:value={firstName}
-          name="firstName"
-          placeholder="Your name..."
-        >
-        <label for="lastName">Last Name</label>
-        <input
-          id="lastName"
-          aria-label="Last Name"
-          type="text"
-          bind:value={lastName}
-          name="lastName"
-          placeholder="Your last name..."
-        >
-        <label for="email">Email</label>
-        <input
-          id="email"
-          aria-label="email"
-          type="email"
-          bind:value={email}
-          name="lastName"
-          placeholder="Your email address..."
-        >
-        <label for="message">Subject</label>
-        <textarea
-          id="message"
-          aria-label="message"
-          type="text"
-          bind:value={message}
-          name="message"
-          placeholder="Write something..."
-        ></textarea>
-        <input type="submit" value="Submit">
-      </form>
-    </div>
-<!--   {/if} -->
+  <div>
+    <form on:submit|preventDefault={submitHandler}>
+      <p>Send a message</p>
+      <!-- First Name -->
+      <label for="firstName">First Name</label>
+      <input
+        id="firstName"
+        aria-label="First Name"
+        type="text"
+        bind:value={fields.firstName}
+        name="firstName"
+        placeholder="Your name..."
+      >
+      <div class="errors">{ errors.firstName }</div>
+      <!-- Last Name -->
+      <label for="lastName">Last Name</label>
+      <input
+        id="lastName"
+        aria-label="Last Name"
+        type="text"
+        bind:value={fields.lastName}
+        name="lastName"
+        placeholder="Your last name..."
+      >
+      <div class="errors">{ errors.lastName }</div>
+      <!-- Email -->
+      <label for="email">Email</label>
+      <input
+        id="email"
+        aria-label="Email"
+        type="email"
+        bind:value={fields.email}
+        name="email"
+        placeholder="Your email address..."
+      >
+      <div class="errors">{ errors.email }</div>
+      <!-- Message -->
+      <label for="message">Message</label>
+      <textarea
+        id="message"
+        aria-label="message"
+        type="text"
+        bind:value={fields.message}
+        name="message"
+        placeholder="Write something..."
+      ></textarea>
+      <div class="errors">{ errors.message }</div>
+      <!-- Submit -->
+      <input type="submit" value="Submit">
+    </form>
+  </div>
 </section>
 
 <style>
@@ -91,5 +103,8 @@ label {
 }
 textarea {
   height: 200px;
+}
+.errors {
+  color: red;
 }
 </style>
