26c425bbe0f86f51832c7dcf454b54a298f9460d
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-02-15 11:07:11 +0000

    refactor: change api call to match sanity docs

--- dev/api/comment.js
+++ dev/api/comment.js
@@ -2,26 +2,26 @@ require("dotenv").config()
 const sanityClient = require("@sanity/client")
 
 async function sendComment(req, res) {
+  const client = sanityClient.config({
+    projectId: process.env.SANITY_PROJECT_ID,
+    dataset: "production",
+    apiVersion: "2021-02-11",
+    token: process.env.SANITY_API_TOKEN,
+    useCdn: true
+  })
+  const doc = {
+    _type: "comment",
+    email: req.body.email,
+    message: req.body.message,
+    name: req.body.name,
+    post: {
+      _ref: req.body.id,
+      _type: "reference"
+    },
+    twitterHandle: req.body.twitterHandle
+  }
   try {
-    await sanityClient.config({
-      projectId: process.env.SANITY_PROJECT_ID,
-      dataset: "production",
-      apiVersion: "2021-02-11",
-      token: process.env.SANITY_API_TOKEN,
-      useCdn: true
-    })
-    .create({
-      _type: "comment",
-      email: req.body.email,
-      message: req.body.message,
-      name: req.body.name,
-      post: {
-        _ref: req.body.id,
-        _type: "reference"
-      },
-      twitterHandle: req.body.twitterHandle
-    })
-    .then(res => {
+    await client.create(doc).then((res) => {
       console.log(`Comment was created, document ID is ${res._id}`)
     })
   } catch (error) {
