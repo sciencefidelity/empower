1a3dcdc8cdfad3a6fb08cf1df6e8af83ad873670
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-02-15 10:28:00 +0000

    fix: change import statement to ES6

--- dev/api/comment.js
+++ dev/api/comment.js
@@ -1,27 +1,27 @@
 require("dotenv").config()
-const sanityClient = require('@sanity/client')
-
-sanityClient.config({
-  projectId: process.env.PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.PUBLIC_SANITY_DATASET,
-  apiVersion: process.env.PUBLIC_SANITY_API_VERSION,
-  token: process.env.SANITY_API_TOKEN,
-  useCdn: true
-})
+import sanityClient from "@sanity/client"
 
 async function sendComment(req, res) {
   try {
-    await sanityClient.create({
-        _type: "comment",
-        email: req.body.email,
-        message: req.body.message,
-        name: req.body.name,
-        post: {
-          _ref: req.body.id,
-          _type: "reference"
-        },
-        twitterHandle: req.body.twitterHandle
-      }).then(res => {
+    await sanityClient.config({
+      projectId: process.env.PUBLIC_SANITY_PROJECT_ID,
+      dataset: process.env.PUBLIC_SANITY_DATASET,
+      apiVersion: process.env.PUBLIC_SANITY_API_VERSION,
+      token: process.env.SANITY_API_TOKEN,
+      useCdn: true
+    })
+    .create({
+      _type: "comment",
+      email: req.body.email,
+      message: req.body.message,
+      name: req.body.name,
+      post: {
+        _ref: req.body.id,
+        _type: "reference"
+      },
+      twitterHandle: req.body.twitterHandle
+    })
+    .then(res => {
       console.log(`Comment was created, document ID is ${res._id}`)
     })
   } catch (error) {
