8ecdd2147b6f39f2ca10b5b4ccc37fddd090c845
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-03-30 21:42:46 -0200

    refactor: test a server only env variable

--- dev/src/env.d.ts
+++ dev/src/env.d.ts
@@ -2,7 +2,7 @@
 // https://vitejs.dev/guide/env-and-mode.html#intellisense-for-typescript
 
 interface ImportMetaEnv {
-  readonly PUBLIC_SANITY_PROJECT_ID: string
+  readonly SANITY_PROJECT_ID: string
   readonly PUBLIC_SANITY_DATASET: string
   readonly PUBLIC_SANITY_API_VERSION: string
 }
