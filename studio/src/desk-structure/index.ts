04a5885f639cdd9d9164b50c007fe42daed51278
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-04-08 22:45:23 +0100

    refactor: improve the desk structure

--- studio/src/desk-structure/index.ts
+++ studio/src/desk-structure/index.ts
@@ -2,18 +2,31 @@ import S from '@sanity/desk-tool/structure-builder'
 import Facebook from '../components/Facebook'
 import Google from '../components/Google'
 import Twitter from '../components/Twitter'
-import { HiOutlineColorSwatch } from 'react-icons/hi'
 import {
-  RiBook2Line,
-  RiEdit2Line,
-  RiNavigationLine,
-  RiPriceTag3Line,
-  RiSettings2Line,
-  RiTeamLine
-} from 'react-icons/ri'
+  BallotBoxWithCheck,
+  Books,
+  CameraFlash,
+  Compass,
+  FilmProjector,
+  Gear,
+  Label,
+  Lotus,
+  LotusPosition,
+  SpeechBalloon,
+  WhiteCheckMark,
+  WritingHand
+} from '../components/twemoji'
+
 
 export const getDefaultDocumentNode = ({ schemaType }) => {
-  if (schemaType === "post" || schemaType === "page" || schemaType === "tag") {
+  if (
+    schemaType === "author" ||
+    schemaType === "page" ||
+    schemaType === "post" ||
+    schemaType === "section" ||
+    schemaType === "tag" ||
+    schemaType === "video"
+  ) {
     return S.document().views([
       S.view.form(),
       S.view.component(Google).title('Google'),
@@ -29,156 +42,83 @@ export default () =>
     .items([
       S.listItem()
         .title('Post')
-        .icon(RiEdit2Line)
+        .icon(WritingHand)
         .child(
-          S.list()
-            .title('Filters')
-            .items([
-              S.listItem()
-                .title('All posts')
-                .icon(RiEdit2Line)
-                .child(
-                  S.documentTypeList('post')
-                    .title('All posts')
-                    .filter('_type == "post"')
-                ),
-              S.listItem()
-                .title('Draft posts')
-                .icon(RiEdit2Line)
-                .child(
-                  S.documentTypeList('post')
-                    .title('Draft')
-                    .filter('_type == "post" && (_id in path("drafts.**"))')
-                ),
-              S.listItem()
-                .title('Published posts')
-                .icon(RiEdit2Line)
-                .child(
-                  S.documentTypeList('post')
-                    .title('Published')
-                    .filter('_type == "post" && !(_id in path("drafts.**"))')
-                ),
-              S.listItem()
-                .title('Featuerd posts')
-                .icon(RiEdit2Line)
-                .child(
-                  S.documentTypeList('post')
-                    .title('Featuerd')
-                    .filter('_type == "post" && feature == true')
-                ),
-              S.listItem()
-                .title('Posts by tag')
-                .icon(RiPriceTag3Line)
-                .child(
-                  S.documentTypeList('tag')
-                    .title('Tag')
-                    .child(tagId =>
-                      S.documentList()
-                        .title('Posts')
-                        .filter(
-                          '_type == "post" && $tagId in settings.tags[]._ref'
-                        )
-                        .params({ tagId })
-                    )
-                ),
-              S.listItem()
-                .title('Posts by author')
-                .icon(RiTeamLine)
-                .child(
-                  S.documentTypeList('author')
-                    .title('Author')
-                    .child(authorId =>
-                      S.documentList()
-                        .title('Posts')
-                        .filter(
-                          '_type == "post" && $authorId in settings.authors[]._ref'
-                        )
-                        .params({ authorId })
-                    )
-                )
-            ])
+          S.documentTypeList('post')
+            .title('Post')
         ),
       S.listItem()
         .title('Page')
-        .icon(RiBook2Line)
+        .icon(Books)
+        .child(
+          S.documentTypeList('page')
+            .title('Page')
+        ),
+      S.listItem()
+        .title('Section')
+        .icon(Lotus)
+        .child(
+          S.documentTypeList('section')
+            .title('Section')
+        ),
+      S.listItem()
+        .title('Video')
+        .icon(FilmProjector)
+        .child(
+          S.documentTypeList('video')
+            .title('Video')
+        ),
+      S.divider(),
+      S.listItem()
+        .title('Comment')
+        .icon(SpeechBalloon)
         .child(
           S.list()
-            .title('Filters')
+            .title('Inbox')
             .items([
               S.listItem()
-                .title('All pages')
-                .icon(RiBook2Line)
+                .title('Pending')
+                .icon(BallotBoxWithCheck)
                 .child(
-                  S.documentTypeList('page')
-                    .title('All pages')
-                    .filter('_type == "page"')
+                  S.documentTypeList('comment')
+                    .title('Comments pending')
+                    .filter('_type == "comment" && approved != true')
                 ),
               S.listItem()
-                .title('Draft pages')
-                .icon(RiBook2Line)
+                .title('Approved')
+                .icon(WhiteCheckMark)
                 .child(
-                  S.documentTypeList('page')
-                    .title('Draft')
-                    .filter('_type == "page" && (_id in path("drafts.**"))')
-                ),
-              S.listItem()
-                .title('Published pages')
-                .icon(RiBook2Line)
-                .child(
-                  S.documentTypeList('page')
-                    .title('Published')
-                    .filter('_type == "page" && !(_id in path("drafts.**"))')
-                ),
-              S.listItem()
-                .title('Featuerd pages')
-                .icon(RiBook2Line)
-                .child(
-                  S.documentTypeList('page')
-                    .title('Featuerd')
-                    .filter('_type == "page" && feature == true')
-                ),
-              S.listItem()
-                .title('Pages by tag')
-                .icon(RiPriceTag3Line)
-                .child(
-                  S.documentTypeList('tag')
-                    .title('Tag')
-                    .child(tagId =>
-                      S.documentList()
-                        .title('Pages')
-                        .filter(
-                          '_type == "page" && $tagId in settings.tags[]._ref'
-                        )
-                        .params({ tagId })
-                    )
-                ),
-              S.listItem()
-                .title('Pages by author')
-                .icon(RiTeamLine)
-                .child(
-                  S.documentTypeList('author')
-                    .title('Author')
-                    .child(authorId =>
-                      S.documentList()
-                        .title('Pages')
-                        .filter(
-                          '_type == "page" && $authorId in settings.authors[]._ref'
-                        )
-                        .params({ authorId })
-                    )
+                  S.documentTypeList('comment')
+                    .title('Comments approved')
+                    .filter('_type == "comment" && approved == true')
                 )
             ])
         ),
-      ...S.documentTypeListItems().filter(
-        item =>
-          !['settings', 'design', 'navigation', 'page', 'post'].includes(
-            item.getId()
-          )
-      ),
+      S.listItem()
+        .title('Author')
+        .icon(LotusPosition)
+        .child(
+          S.documentTypeList('author')
+            .title('Author')
+        ),
+      S.listItem()
+        .title('Photography')
+        .icon(CameraFlash)
+        .child(
+          S.documentTypeList('photography')
+            .title('Photography')
+        ),
+      S.listItem()
+        .title('Tag')
+        .icon(Label)
+        .child(
+          S.documentTypeList('tag')
+            .title('Tag')
+        ),
       S.divider(),
       S.listItem()
         .title('settings')
-        .icon(RiSettings2Line)
+        .icon(Gear)
         .child(
           S.list()
             .title('Settings')
@@ -188,17 +128,30 @@ export default () =>
                 .child(
                   S.document().schemaType('settings').documentId('settings')
                 )
-                .icon(RiSettings2Line),
-              S.listItem()
-                .title('Design')
-                .child(S.document().schemaType('design').documentId('design'))
-                .icon(HiOutlineColorSwatch),
+                .icon(Gear),
               S.listItem()
                 .title('Navigation')
                 .child(
                   S.document().schemaType('navigation').documentId('navigation')
                 )
-                .icon(RiNavigationLine)
+                .icon(Compass)
             ])
-        )
+        ),
+      ...S.documentTypeListItems().filter(
+        item =>
+          ![
+            'author',
+            'comment',
+            'navigation',
+            'page',
+            'photography',
+            'post',
+            'section',
+            'settings',
+            'tag',
+            'video'
+          ].includes(
+            item.getId()
+          )
+      ),
     ])
